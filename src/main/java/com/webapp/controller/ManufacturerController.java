c0ce5347f1b7de4d770d75f858e16eaa686fd196
Admin-Komputer\Admin <ehe241@gmail.com>
2018-09-22 14:12:15 +0200

    Added ManufacturerController

--- src/main/java/com/webapp/controller/ManufacturerController.java
+++ src/main/java/com/webapp/controller/ManufacturerController.java
@@ -1,15 +1,35 @@
 package com.webapp.controller;
 
 
+import com.webapp.model.entity.Manufacturer;
+import com.webapp.service.ManufacturerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/admin")
 public class ManufacturerController {
 
+    @Autowired
+    private ManufacturerService manufacturerService;
+
     @RequestMapping("/manufacturer")
     public String getManufacturerPage(){
         return "admin/manufacturer";
+    }
+
+    @RequestMapping(value = "/manufacturer", method = RequestMethod.POST)
+    public @ResponseBody Manufacturer
+            addManufacturer(@RequestBody Manufacturer manufacturer, HttpServletResponse response){
+            Manufacturer returned = manufacturerService.addManufacturer(manufacturer);
+            if(returned==null)
+                response.setStatus(HttpServletResponse.SC_CONFLICT);
+            return returned;
     }
 }
