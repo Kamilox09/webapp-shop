44448e285df75895d7dc6445a73d2d531be94b06
Admin-Komputer\Admin <ehe241@gmail.com>
2018-09-26 11:46:34 +0200

    Added CategoryService and implementation

--- src/main/java/com/webapp/dao/impl/CategoryDaoImpl.java
+++ src/main/java/com/webapp/dao/impl/CategoryDaoImpl.java
@@ -3,7 +3,11 @@ package com.webapp.dao.impl;
 import com.webapp.dao.CategoryDao;
 import com.webapp.model.entity.Category;
 import org.hibernate.query.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Repository("categoryDao")
+@Transactional
 public class CategoryDaoImpl extends AbstractDao<Category> implements CategoryDao {
     @Override
     public Category getCategoryByName(String name) {
--- src/main/java/com/webapp/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/webapp/service/impl/CategoryServiceImpl.java
@@ -1,12 +1,54 @@
 package com.webapp.service.impl;
 
 
+import com.webapp.dao.CategoryDao;
+import com.webapp.model.entity.Category;
 import com.webapp.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service("categoryService")
 @Transactional
 public class CategoryServiceImpl implements CategoryService {
 
+    private final CategoryDao categoryDao;
+
+    @Autowired
+    public CategoryServiceImpl(CategoryDao categoryDao){
+        this.categoryDao=categoryDao;
+    }
+
+    @Override
+    public Category addCategory(Category category) {
+        Category check = categoryDao.getCategoryByName(category.getName());
+        if(check!=null)
+            return null;
+        return categoryDao.create(category);
+    }
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categoryDao.getAll();
+    }
+
+    @Override
+    public Category getCategoryById(long id) {
+        return categoryDao.getByKey(id);
+    }
+
+    @Override
+    public Category updateCategory(Category category) {
+        Category check = categoryDao.getCategoryByName(category.getName());
+        if(check!=null)
+            return null;
+        return categoryDao.update(category);
+    }
+
+    @Override
+    public void deleteCategoryById(long id) {
+        categoryDao.deleteByKey(id);
+    }
 }
