bf87b713156139e6426e020259746f50ca5f44be
Admin-Komputer\Admin <ehe241@gmail.com>
2018-10-10 13:14:18 +0200

    Added PhotoDao

---
+++ src/main/java/com/webapp/dao/PhotoDao.java
@@ -0,0 +1,7 @@
+package com.webapp.dao;
+
+import com.webapp.model.entity.Photo;
+
+public interface PhotoDao extends IAbstractDao<Photo> {
+
+}
---
+++ src/main/java/com/webapp/dao/impl/PhotoDaoImpl.java
@@ -0,0 +1,7 @@
+package com.webapp.dao.impl;
+
+import com.webapp.dao.PhotoDao;
+import com.webapp.model.entity.Photo;
+
+public class PhotoDaoImpl extends AbstractDao<Photo> implements PhotoDao {
+}
